617f24645b41fdfc47a397af2ba21a3a1663b86e
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2022-11-01 00:57:05 +0530

    React-JSX-001
    Simple React JSX

---
+++ React-JSX-001/about_jsx/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 >About us</h1>
+        <p>He decided water-skiing on a frozen lake wasn’t a good idea.</p>
+        <p>
+          "Begin today!" That's all the note said. There was no indication from
+          where it came or who may have written it. Had it been meant for
+          someone else? Meghan looked around the room, but nobody made eye
+          contact back. For a brief moment, she thought it might be a message
+          for her to follow her dreams, but ultimately decided it was easier to
+          ignore it as she crumpled it up and threw it away. Was it enough? That
+          was the question he kept asking himself. Was being satisfied enough?
+          He looked around him at everyone yearning to just be satisfied in
+          their daily life and he had reached that goal. He knew that he was
+          satisfied and he also knew it wasn't going to be enough.
+        </p>
+        <p>
+          you're a programmer and you need random text to test the program,
+          using these paragraphs can be the perfect way to do this. Anyone who's
+          in search of realistic text for a project can use one or more of these
+          random paragraphs to fill their need._________.
+        </p>
+      </header>
+      {/* <main></main> */}
+    </div>
+  );
+}
+
+export default App;
