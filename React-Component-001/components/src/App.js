f54cd13afe8f84c75d1fb3ae711d6ba746045ecb
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2022-11-02 10:58:45 +0530

    simple react components

---
+++ React-Component-001/components/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import Compo from "./Components/Compo";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <Compo />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-Component-001/components/src/Components/Compo.js
@@ -0,0 +1,55 @@
+import React from "react";
+export default function Compo() {
+  return (
+    <div>
+      <div>
+        <h3>Bittoo Agrawal</h3>
+        <p>Software Developer,Educator at Geekster </p>
+        <a link href="">
+          Linkedin
+        </a>
+        <a link href="">
+          GitHub
+        </a>
+        <hr />
+      </div>
+      <div>
+        <h4>Experience</h4>
+        <p>Software Developer at Amazon</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <p>Software Developer at Amazon</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <h2>Skills</h2>
+        <ul>
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>API</li>
+          <li>React</li>
+          <li>System design</li>
+        </ul>
+        <hr />
+        <h2>Hobbies</h2>
+        <ol>
+          <li>Table tennis</li>
+          <li>Reading Books</li>
+          <li>Biography</li>
+          <li>Teaching</li>
+        </ol>
+      </div>
+    </div>
+  );
+}
