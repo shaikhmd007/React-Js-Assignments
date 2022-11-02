4f4b29d56267e39e659e7cea65a8044c054bd1b4
Mohammad Shaikh <94097592+shaikhmd007@users.noreply.github.com>
2022-11-02 10:45:50 +0530

    Learn JSX

---
+++ React-JSX-002/jsx/src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Evolution of Airbnb's Frontend</h1>
+        <p>@spikebrehm</p>
+        <img src="Airbnbs.jpg" alt="Inc.logo" style={{ height: "40px" }} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
